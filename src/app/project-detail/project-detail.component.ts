6c21393b70d8cee07142b9b4b89e899f25dd90b4
Xavier Trinidad <xmtrinidad@gmail.com>
2018-08-11 09:31:20 -0700

    Added framework for project-detail with some routing

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from '../services/project.service';
+import { Project } from '../models/project.model';
+import { ActivatedRoute } from '../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-project-detail',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-detail.component.css']
 })
 export class ProjectDetailComponent implements OnInit {
+  project: Project;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private projectService: ProjectService) { }
 
   ngOnInit() {
+    const selectedProjectId = +this.route.snapshot.paramMap.get('id');
+    this.project = this.projectService.getProject(selectedProjectId);
   }
 
 }
--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '../../../node_modules/@angular/router';
 import { PROJECTS } from '../models/mock-data';
 import { Project } from '../models/project.model';
+
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +11,7 @@ import { Project } from '../models/project.model';
 export class ProjectService {
   projects: Project[] = PROJECTS;
   project: Project;
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   /**
    * Get all projects from mock-data.
@@ -16,6 +19,20 @@ export class ProjectService {
    */
   getProjects() {
     return this.projects;
+  }
+
+  /**
+   * Get project to display on project-detail page
+   * @param selectedProjectId - the project selected
+   */
+  getProject(selectedProjectId: number) {
+    // Find the selected project based on id
+    const selectedProject = this.projects.find(project => project.id === selectedProjectId);
+
+    // Update this service with the current project
+    this.project = selectedProject;
+    
+    return selectedProject;
   }
 
   /**
